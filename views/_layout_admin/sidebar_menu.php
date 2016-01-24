fe04d652a2a7af9103f7c6379eea31a94fa1c1ea
LoiNT <ntlzz5793@hotmail.com>
2016-01-25 02:59:39 +0700

    ẩn hết các thẻ html ở trang login và thêm danh mục ở trong slidebar menu của admin -- cắt html slidebar menu, giờ quản lý trong _layout_admin\sidebar_menu.php

---
+++ views/_layout_admin/sidebar_menu.php
@@ -0,0 +1,66 @@
+<ul class="sidebar-menu">
+    <li class="header">MAIN NAVIGATION</li>
+    <li class="active treeview">
+        <a href="<?= ADMIN_ROOT ?>">
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span> </i>
+        </a>
+
+    </li>
+    <li class="treeview">
+        <a href="#">
+            <i class="fa fa-files-o"></i>
+            <span>Catalog</span><i class="fa fa-angle-left pull-right"></i>
+        </a>
+        <ul class="treeview-menu">
+            <li><a href=""><i class="fa fa-circle-o"></i> Catagories<i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#"><i class="fa fa-circle-o"></i>Catagories</a></li>
+                </ul>
+                <ul class="treeview-menu">
+                    <li><a href="#"><i class="fa fa-circle-o"></i>Catagories Posts</a></li>
+                </ul>
+            </li>
+
+        </ul>
+        <ul class="treeview-menu">
+            <li><a href=""><i class="fa fa-circle-o"></i> Products</a></li>
+        </ul>
+        <ul class="treeview-menu">
+            <li><a href=""><i class="fa fa-circle-o"></i> Posts</a></li>
+        </ul>
+        <ul class="treeview-menu">
+            <li><a href=""><i class="fa fa-circle-o"></i> Tags<i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#"><i class="fa fa-circle-o"></i>Tags Posts</a></li>
+                </ul>
+                <ul class="treeview-menu">
+                    <li><a href="#"><i class="fa fa-circle-o"></i>Tags Products</a></li>
+                </ul>
+            </li>
+
+        </ul>
+    </li>
+    <li class="treeview">
+        <a href="#">
+            <i class="fa fa-shopping-cart"></i> <span>Sales</span> </i><i class="fa fa-angle-left pull-right"></i>
+        </a>
+        <ul class="treeview-menu">
+            <li><a href=""><i class="fa fa-circle-o"></i> Receipts<i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#"><i class="fa fa-circle-o"></i>Receipts</a></li>
+                </ul>
+                <ul class="treeview-menu">
+                    <li><a href="#"><i class="fa fa-circle-o"></i>Receipts Detail</a></li>
+                </ul>
+            </li>
+        </ul>
+        <ul class="treeview-menu">
+            <li><a href=""><i class="fa fa-circle-o"></i> Carts</a></li>
+        </ul>
+    </li>
+    <li class="active treeview">
+        <a href="">
+            <i class="fa fa-users"></i> <span>Users</span> </i>
+        </a>
+    </li>
+</ul>
--- webroot/index.php
+++ webroot/index.php
@@ -2,13 +2,16 @@
 
 define('DS', DIRECTORY_SEPARATOR); // dau ' / '
 define('ROOT', dirname(dirname(__FILE__))); // thu muc cha
+
 define('VIEWS_PATH', ROOT . DS . 'views');
 define('WEBROOT_PATH', 'http://localhost:8080/BanhKeoTrangAn/webroot');
-define('ROOT_PATH','http://localhost:8080/BanhKeoTrangAn/');
-define('ADMIN_ROOT','http://localhost:8080/BanhKeoTrangAn/admin');
-define('DEFAULT_ROOT','http://localhost:8080/BanhKeoTrangAn/default');
-//$uri = $_SERVER['REQUEST_URI'];
-//print_r($uri);
+define('ROOT_PATH', 'http://localhost:8080/BanhKeoTrangAn/');
+define('ADMIN_ROOT', 'http://localhost:8080/BanhKeoTrangAn/admin');
+define('DEFAULT_ROOT', 'http://localhost:8080/BanhKeoTrangAn/default');
+
+//slide bar
+define('SLIDEBAR_MENU', dirname(dirname(__FILE__)) . DS . "views\_layout_admin\sidebar_menu.php");
+define('LAYOUT_ADMIN', dirname(dirname(__FILE__)) . DS . "views\_layout_admin");
 
 require_once (ROOT . DS . 'lib' . DS . 'init.php');
 
