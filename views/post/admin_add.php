8df52d64ce8d3125886fb7b848f6d0a8447291b2
BruceleeThanh <nguyentrungthanh602@gmail.com>
2016-02-01 03:59:10 +0700

    Thêm ckeditor cho module post

--- views/post/admin_add.php
+++ views/post/admin_add.php
@@ -1,4 +1,5 @@
-
+<script type="text/javascript" src="<?= WEBROOT_PATH ?>/plugins/ckeditor/sample.js"></script>
+<script type="text/javascript" src="<?= WEBROOT_PATH ?>/plugins/ckeditor/ckeditor.js"></script>
 <style>
     .red-require{
         color: red;
@@ -11,12 +12,12 @@
             <input type="text" name="Title" class="form-control" id="exampleInputEmail1" placeholder="Title" required>
         </div>
         <div class="form-group">
-            <label for="">Content</label><span class="red-require"><strong>*</strong></span>
-            <input type="text" name="Content" class="form-control" id="exampleInputPassword1" placeholder="Content" required>
-        </div>
-        <div class="form-group">
             <label for="">Slug</label><span class="red-require"><strong>*</strong></span>
             <input type="text" name="Slug" class="form-control" id="exampleInputPassword1" placeholder="Slug" required>
+        </div>
+        <div class="form-group">
+            <label for="">Content</label><span class="red-require"><strong>*</strong></span>
+            <textarea class="form-control ckeditor" rows="10" id="dataInput" placeholder="Content" name="Content"></textarea>
         </div>
         <div class="form-group">
             <label for="exampleInputFile">Upload Image</label>
