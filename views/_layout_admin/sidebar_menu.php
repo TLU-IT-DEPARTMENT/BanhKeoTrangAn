3edbdbc75ea4560994548bf5d5331e9c9d6c201f
LoiNT <ntlzz5793@hotmail.com>
2016-02-01 23:43:12 +0700

    sửa menubar admin

--- views/_layout_admin/sidebar_menu.php
+++ views/_layout_admin/sidebar_menu.php
@@ -17,25 +17,37 @@
                 <ul class="treeview-menu">
                     <li><a href="<?= ADMIN_ROOT ?>/category/list/page/1"><i class="fa fa-circle-o"></i>Catagories</a></li>
                 </ul>
-                <ul class="treeview-menu">
-                    <li><a href="#"><i class="fa fa-circle-o"></i>Catagories Posts</a></li>
-                </ul>
+
             </li>
 
         </ul>
         <ul class="treeview-menu">
-            <li><a href=""><i class="fa fa-circle-o"></i> Products</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i> Products<i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#"><i class="fa fa-circle-o"></i>Kind Of Posts</a></li>
+                </ul>
+            </li>
         </ul>
         <ul class="treeview-menu">
-            <li><a href="<?= ADMIN_ROOT ?>/post/list/page/1"><i class="fa fa-circle-o"></i> Posts</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i> Posts<i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="<?= ADMIN_ROOT ?>/post/list/page/1"><i class="fa fa-circle-o"></i>Posts</a></li>
+                </ul>
+                <ul class="treeview-menu">
+                    <li><a href="#"><i class="fa fa-circle-o"></i>Catagories Posts</a></li>
+                </ul>
+            </li>
         </ul>
         <ul class="treeview-menu">
             <li><a href=""><i class="fa fa-circle-o"></i> Tags<i class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
-                    <li><a href="#"><i class="fa fa-circle-o"></i>Tags Posts</a></li>
+                    <li><a href="<?= ADMIN_ROOT ?>/tag/list/page/1"><i class="fa fa-circle-o"></i>Tags </a></li>
                 </ul>
                 <ul class="treeview-menu">
-                    <li><a href="#"><i class="fa fa-circle-o"></i>Tags Products</a></li>
+                    <li><a href="<?= ADMIN_ROOT ?>/tagpost/list/page/1"><i class="fa fa-circle-o"></i>Tags Posts</a></li>
+                </ul>
+                <ul class="treeview-menu">
+                    <li><a href="<?= ADMIN_ROOT ?>/tagproduct/list/page/1"><i class="fa fa-circle-o"></i>Tags Products</a></li>
                 </ul>
             </li>
 
@@ -50,9 +62,7 @@
                 <ul class="treeview-menu">
                     <li><a href="#"><i class="fa fa-circle-o"></i>Receipts</a></li>
                 </ul>
-                <ul class="treeview-menu">
-                    <li><a href="#"><i class="fa fa-circle-o"></i>Receipts Detail</a></li>
-                </ul>
+
             </li>
         </ul>
         <ul class="treeview-menu">
@@ -64,7 +74,7 @@
             <i class="fa fa-users"></i> <span>Users</span> </i>
         </a>
     </li>
-    
+
     <li class="active treeview" >
         <a href="<?= ADMIN_ROOT ?>/pages/statistic">
             <i class="fa fa-bar-chart "></i> <span>Statistic</span> </i>
