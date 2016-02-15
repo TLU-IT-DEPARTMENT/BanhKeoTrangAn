667d3b99b5839a9d1193104e6801a848cd4da484
LoiNT <ntlzz5793@hotmail.com>
2016-02-15 14:58:24 +0700

    logout

--- views/_layout/header.php
+++ views/_layout/header.php
@@ -135,16 +135,49 @@
                     </div>
                 </div>
                 <div class="col-sm-8">
-                    <div class="shop-menu pull-right">
-                        <ul class="nav navbar-nav">
-                            <li><a href="#"><i class="fa fa-user"></i> <?php if (Session::get("Name")) echo Session::get("Name");
-else echo "Account"; ?></a></li>
-                            <li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>
-                            <li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>
-                            <li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Cart</a></li>                  
-                            <li><a id="modal_trigger" class="btn <?php if (Session::get("Name")) echo "hidden"; ?>" href="#modal"><i class="fa fa-lock"></i>Login</a></li> 
-                        </ul>
-                    </div>
+                    <form method="post">
+                        <div class="shop-menu pull-right">
+                            <ul class="nav navbar-nav">
+                                <li><a href="#"><i class="fa fa-user"></i> <?php
+                                        if (Session::get("Name"))
+                                            echo Session::get("Name");
+                                        else
+                                            echo "Account";
+                                        ?></a></li>
+                                <li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>
+                                <li><a href="checkout.html"><i class="fa fa-crosshairs"></i> Checkout</a></li>
+                                <li><a href="cart.html"><i class="fa fa-shopping-cart"></i> Cart</a></li>                  
+                                <li><a id="modal_trigger" class="btn <?php if (Session::get("Name")) echo "hidden"; ?>" href="#modal"><i class="fa fa-lock"></i>Login</a></li>
+                                <?php if (Session::get("Name")) echo "<li><button id='logout' class='btn flat' name='logout' onmouseover='changeColor(this);'><i class='fa fa-lock'></i>Logout</button></li>"; ?>
+                                <?php
+                                if (isset($_POST['logout'])) {
+                                    Session::destroy();
+                                    Router::redirect(ROOT_PATH);
+                                }
+                                ?>
+                                <style>
+                                    #logout{
+                                        background: transparent;
+                                        border: none !important;
+                                    }
+                                </style>
+                                <script type="text/javascript" language="javascript">
+
+                                    window.onload = function () {
+                                        document.getElementById("logout").onmouseover = function ()
+                                        {
+                                            this.style.backgroundColor = "#FF9933";
+                                        }
+
+                                        document.getElementById("logout").onmouseout = function ()
+                                        {
+                                            this.style.backgroundColor = "#FFFFFF";
+                                        }
+                                    }
+                                </script>
+                            </ul>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
@@ -317,32 +350,32 @@ else echo "Account"; ?></a></li>
 <script type="text/javascript" src="<?= WEBROOT_PATH ?>/js/jquery.leanModal.min.js"></script>
 
 <script type="text/javascript">
-    $("#modal_trigger").leanModal({top: 200, overlay: 0.6, closeButton: ".modal_close"});
+                                    $("#modal_trigger").leanModal({top: 200, overlay: 0.6, closeButton: ".modal_close"});
 
-    $(function () {
-        // Calling Login Form
-        $("#login_form").click(function () {
-            $(".social_login").hide();
-            $(".user_login").show();
-            return true;
-        });
+                                    $(function () {
+                                        // Calling Login Form
+                                        $("#login_form").click(function () {
+                                            $(".social_login").hide();
+                                            $(".user_login").show();
+                                            return true;
+                                        });
 
-        // Calling Register Form
-        $("#register_form").click(function () {
-            $(".social_login").hide();
-            $(".user_register").show();
-            $(".header_title").text('Register');
-            return true;
-        });
+                                        // Calling Register Form
+                                        $("#register_form").click(function () {
+                                            $(".social_login").hide();
+                                            $(".user_register").show();
+                                            $(".header_title").text('Register');
+                                            return true;
+                                        });
 
-        // Going back to Social Forms
-        $(".back_btn").click(function () {
-            $(".user_login").hide();
-            $(".user_register").hide();
-            $(".social_login").show();
-            $(".header_title").text('Login');
-            return true;
-        });
+                                        // Going back to Social Forms
+                                        $(".back_btn").click(function () {
+                                            $(".user_login").hide();
+                                            $(".user_register").hide();
+                                            $(".social_login").show();
+                                            $(".header_title").text('Login');
+                                            return true;
+                                        });
 
-    })
+                                    })
 </script>
